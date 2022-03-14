78d1516848495093cf762911eedcbe2ed1dd5fc7
amio <cse_2012020268@lus.ac.bd>
2022-03-15 00:20:25 +0600

    userinterface

--- src/amio/RegisterForm.java
+++ src/amio/RegisterForm.java
@@ -2,66 +2,106 @@ package amio;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class RegisterForm  extends JFrame {
 
-    JLabel l1,l2,l3,l4;
-    JTextField t1,t2,t3,t4;
-    JButton SubMitButton;
+
+    JLabel l1,l2;
+    JTextField t1,t2;
+    JButton C,F,K;
     RegisterForm(){
-        setSize(400,400);
+        setSize(470,380);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(null);
         setLocationRelativeTo(null);
+        setTitle("Currency Converter");
+
+
 
         Font f = new Font("Arial",1,20);
 
-        l1 = new JLabel("User name:");
-        l2 = new JLabel("Phone no:");
-        l3 = new JLabel("Gender:");
-        l4 = new JLabel("Email:");
 
 
-        l1.setBounds(20,20,120,30);
+
+        l1 = new JLabel("input in BDT   :           ");
+        l2 = new JLabel("Output            :");
+
+        l1.setBounds(20,70,210,30);
         l1.setFont(f);
-        l2.setBounds(20,80,100,30);
+        l2.setBounds(20,130,200,30);
         l2.setFont(f);
-        l3.setBounds(20,140,100,30);
-        l3.setFont(f);
-        l4.setBounds(20,200,100,30);
-        l4.setFont(f);
+
 
         add(l1);
         add(l2);
-        add(l3);
-        add(l4);
 
 
         t1 = new JTextField();
         t2 = new JTextField();
-        t3 = new JTextField();
-        t4 = new JTextField();
 
-        t1.setBounds(150,20,140,30);
+        t1.setBounds(200,70,200,30);
         t1.setFont(f);
-        t2.setBounds(150,80,140,30);
+        t2.setBounds(200,130,240,30);
         t2.setFont(f);
-        t3.setBounds(150,140,140,30);
-        t3.setFont(f);
-        t4.setBounds(150,200,140,30);
-        t4.setFont(f);
+        t2.setEditable(false);
+
 
         add(t1);
         add(t2);
-        add(t3);
-        add(t4);
 
-        SubMitButton = new JButton("SUBMIT");
-        SubMitButton.setFont(f);
-        SubMitButton.setBounds(250,270,120,30);
-        SubMitButton.setBackground(Color.CYAN);
-        add(SubMitButton);
 
+        K = new JButton("AUS");
+        K.setFont(f);
+        K.setBounds(290,280,100,30);
+        K.setBackground(Color.BLUE);
+        K.setForeground(Color.WHITE);
+        add(K);
+
+        F = new JButton("USA");
+        F.setFont(f);
+        F.setBounds(170,280,100,30);
+        F.setBackground(Color.yellow);
+        add(F);
+
+        C = new JButton("IND");
+        C.setFont(f);
+        C.setBounds(50,280,100,30);
+        C.setBackground(Color.RED);
+        C.setForeground(Color.WHITE);
+        add(C);
+
+        C.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                double d = Double.parseDouble(t1.getText());
+                double d1 = (d / 1.16);
+                String str1 = String.valueOf(d1);
+                t2.setText(str1);
+            }
+        });
+        F.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double d = Double.parseDouble(t1.getText());
+                double d2 = (d/85.89);
+                String str2 = String.valueOf(d2);
+                t2.setText(str2);
+
+            }
+        });
+        K.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double d = Double.parseDouble(t1.getText());
+                double d3 = (d/61.77);
+                String str3 = String.valueOf(d3);
+                t2.setText(str3);
+            }
+        });
 
 
 
@@ -70,4 +110,7 @@ public class RegisterForm  extends JFrame {
 
         setVisible(true);
     }
+
+
+
 }
